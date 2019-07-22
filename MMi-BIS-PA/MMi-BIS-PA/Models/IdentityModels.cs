3ad34a7e7672a59152c666889f10a8548d29e711
Tejas Kuthe <tejas17kuthe@gmail.com>
2019-07-22 11:22:45 +0530

    added comment

--- MMi-BIS-PA/MMi-BIS-PA/Models/IdentityModels.cs
+++ MMi-BIS-PA/MMi-BIS-PA/Models/IdentityModels.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MMi_BIS_PA.Models
-{
+{   //edited by tejas
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
