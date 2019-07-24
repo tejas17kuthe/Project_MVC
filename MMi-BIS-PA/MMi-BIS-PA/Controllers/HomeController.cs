432fcc38952bb4bd84f3e8247fb72caa8f2adb09
Tejas Kuthe <tejas17kuthe@gmail.com>
2019-07-24 13:30:52 +0530

    homeController login page authentication code using httpGet and httpPost attributes.

--- MMi-BIS-PA/MMi-BIS-PA/Controllers/HomeController.cs
+++ MMi-BIS-PA/MMi-BIS-PA/Controllers/HomeController.cs
@@ -1,22 +1,31 @@
-﻿using System;
+﻿
+using MMi_BIS_PA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MMi_BIS_PA.Models;
+
 
 namespace MMi_BIS_PA.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string user_name,string password)
         {
             MySqlDatabaseInteraction connection = new MySqlDatabaseInteraction();
 
-            bool result = connection.AuthenticateUser("tejas", "kuthe");
+            bool result = connection.AuthenticateUser(user_name,password);
 
             if (result)
-                return View();
+                return RedirectToRoute;
             else
                 return Content("Authentication error");
         }
