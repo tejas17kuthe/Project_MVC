650af97f2d9f349930896dbe89cf3e2b6358c602
Tejas Kuthe <tejas17kuthe@gmail.com>
2019-07-25 11:27:12 +0530

    home controller conflict resolved kept source file

--- MMi-BIS-PA/MMi-BIS-PA/Controllers/HomeController.cs
+++ MMi-BIS-PA/MMi-BIS-PA/Controllers/HomeController.cs
@@ -1,26 +1,45 @@
-﻿using System;
+﻿
+using MMi_BIS_PA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+<<<<<<< HEAD
+
+=======
 using MMi_BIS_PA.Models;
 using MMi_BIS_PA.Models;
+>>>>>>> origin/EntityFramework_codeFirst
 
 namespace MMi_BIS_PA.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+<<<<<<< HEAD
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Index(string user_name,string password)
+        {
+            MySqlDatabaseInteraction connection = new MySqlDatabaseInteraction();
 
-            if (new MySqlDatabaseInteraction().AuthenticateUser("tejas", "kute"))
-                return View();
+            bool result = connection.AuthenticateUser(user_name,password);
+
+            if (result)
+                return RedirectToRoute(new { Controller="CurrentDataPage", Action= "CurrentDataPage" });
             else
-                return Content("Authentication error");
+                return View();
+=======
+
 
            
 
+>>>>>>> origin/EntityFramework_codeFirst
         }
 
         public ActionResult About()
